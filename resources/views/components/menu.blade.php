3b2b393733d9b8c18646af3559721ad65a7fb7e8
jpgerber1986 <jordangerber@gmail.com>
2025-02-17 17:40:08 -0700

    Adjusted Menu and styles

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -4,5 +4,10 @@
         <span class="a">Home</span>
     </a>
 
+    <a href="#practitioners" {{-- wire:navigate --}}>
+        <i class="nf nf-fa-stethoscope"></i>
+        <span class="a">Practitioners</span>
+    </a>
+
     @livewire('components.logout')
 </nav>
